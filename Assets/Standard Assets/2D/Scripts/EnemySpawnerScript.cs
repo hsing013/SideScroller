258b05bd722ca02b669d4cc69863224472a5372f
hsing013 <hsing013@ucr.edu>
2019-04-20 14:07:16 -0400

    Spawning Enemies

---
+++ Assets/Standard Assets/2D/Scripts/EnemySpawnerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject enemy;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Instantiate(enemy, new Vector2(transform.position.x, transform.position.y), Quaternion.identity);
+        
+    }
+}
